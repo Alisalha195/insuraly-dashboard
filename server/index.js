669640eb834703580a69b39fff53b296b1af2b79
Alisalha195 <alisalha195@gmail.com>
2025-04-23 09:08:58 -0700

    init repo

---
+++ server/index.js
@@ -0,0 +1,18 @@
+
+import express from 'express';
+import dotenv from "dotenv"
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+try {
+   // connectDB();
+   app.listen(port , () => {console.log(`Server is running on port ${port}`)})
+ 
+ } catch(error) {
+   console.log(error)
+ }
