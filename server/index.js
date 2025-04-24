869be6baa1bc592d6ad145665d1cb65b8c264519
Alisalha195 <alisalha195@gmail.com>
2025-04-24 01:59:19 -0700

    add supabase

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 
 import express from 'express';
 import dotenv from "dotenv"
-import path from "path";
+import cors from "cors"; 
+import {supabase} from "./db/connection.js"
 
 dotenv.config();
-
 const app = express();
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 try {
-   // connectDB();
    app.listen(port , () => {console.log(`Server is running on port ${port}`)})
- 
+   console.log("supabase is : ",supabase)
  } catch(error) {
    console.log(error)
  }
