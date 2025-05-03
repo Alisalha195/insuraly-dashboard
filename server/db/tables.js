339cebed968333205f663eb0e53d0c57b51d7cbb
Alisalha195 <alisalha195@gmail.com>
2025-05-03 02:53:55 -0700

    add tables file

---
+++ server/db/tables.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export const personalInfoTable = process.env.PERSONAL_INFORMATIONS_TABLE
+export const employeeTable = process.env.EMPLOYEES_TABLE
+export const businessTable = process.env.BUSINESS_TABLE
+export const businessOwnerTable = process.env.BUSINESS_OWNERS_TABLE
+export const usersTable = process.env.USERS_TABLE
+export const businessEmployeeTable = process.env.BUSINESS_EMPLOYEE_TABLE
+export const claimsTable = process.env.CLAIMS_TABLE
+export const retirementTable = process.env.RETIREMENT_TABLE
+export const retirementIndemnificationTable = process.env.RETIREMENT_INDEMNIFICATION_TABLE
+export const retirementSalaryTable = process.env.RETIREMENT_SALARY_TABLE
+export const retirementSalaryAuthorizedTable = process.env. RETIREMENT_SALARY_AUTHORIZED_TABLE
+export const retirementSalarySuccessorTable = process.env.RETIREMENT_SALARY_SUCCESSOR_TABLE
+export const servicesTable = process.env.SERVICES_TABLE
+export const servicesWagesTable = process.env.SERVICES_WAGES_TABLE
+export const unpaidVacationsTable = process.env.UNPAID_VACATIONS_TABLE
