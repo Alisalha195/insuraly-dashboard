89647277a75dc867ee6f5ebc5cc79faa0b8ace11
Alisalha195 <alisalha195@gmail.com>
2025-05-31 01:23:00 -0700

    add dashboard page

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,8 +1,16 @@
 
+import { Box, Flex, Stack } from "@chakra-ui/react";
+import Header from "../../components/dashboard/header/Header";
+import MainContent from "../../components/dashboard/content/MainContent";
+import Footer from "../../components/dashboard/footer/Footer";
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+   <Stack gap={0}  >
+      <MainContent />
+      <Footer />
+   </Stack>
   )
 }
 
-export default Dashboard
+export default Dashboard;
