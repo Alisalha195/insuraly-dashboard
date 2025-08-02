762c9f49bacc2912569923212eab6f7692a04c56
jhon911911 <alisalha195@gmail.com>
2025-08-02 11:09:31 +0400

    fix

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,7 @@ import {signup , login , logout} from "../controllers/authController.js"
 import { authenticated } from "../middlewares/authenticated.js";
 import {createUser} from "../controllers/userController.js";
 
-const router = Router(); 
+const router = Router();  
 router.post("/signup", signup, createUser);
 router.post("/login", login);
 router.post("/logout",authenticated, logout);
