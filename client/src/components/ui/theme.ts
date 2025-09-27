3f2f5bb7adc8d92eb4a738b9fe5bc2240aae64d1
Alih Salha <82464453+Alisalha195@users.noreply.github.com>
2025-09-27 07:53:58 +0000

    fix chakra theme config

--- client/src/components/ui/theme.ts
+++ client/src/components/ui/theme.ts
@@ -8,22 +8,23 @@ const config = defineConfig({
                "500": { value: "blue" },
             },
          },
-         sizes: {
-            "1/7": { value: "14.285%" },
-            xs: { value: "4px" },
+         // sizes: {
+         //    "1/7": { value: "14.285%" },
+         //    xs: { value: "4px" },
 
-            sm: { value: "4px" }, // ~480px
-            // md: "48rem", // ~768px
-            // lg: "62rem", // ~992px
-            // xl: "80rem", // ~1280px
-            // "2xl": "96rem", // ~1536px
-            // "3xl": "120em",
-            // "4xl": "160em",
-         }
+         //    sm: { value: "4px" }, // ~480px
+         //    // md: "48rem", // ~768px
+         //    // lg: "62rem", // ~992px
+         //    // xl: "80rem", // ~1280px
+         //    // "2xl": "96rem", // ~1536px
+         //    // "3xl": "120em",
+         //    // "4xl": "160em",
+         // }
       },
       breakpoints: {
          base: "0rem", // 0px
          "xss": "2rem",
+         "xs" : "10rem" ,
          sm: "30rem", // ~480px
          md: "48rem", // ~768px
          lg: "62rem", // ~992px
@@ -35,4 +36,4 @@ const config = defineConfig({
    },
 });
 
-export const system = createSystem(defaultConfig, config);
+export const system = createSystem( defaultConfig, config);
