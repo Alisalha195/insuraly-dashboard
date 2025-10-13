f6335c877538b00ea30521a0c835890e16ced7fc
Alih Salha <82464453+Alisalha195@users.noreply.github.com>
2025-10-13 16:33:06 +0000

    fix:business owner responsive profile

--- client/src/components/shared/profile/businessOwner/BusinessOwnerProfileTabs.tsx
+++ client/src/components/shared/profile/businessOwner/BusinessOwnerProfileTabs.tsx
@@ -1,32 +1,34 @@
-import { Tabs } from "@chakra-ui/react"
+import { Tabs, Box } from "@chakra-ui/react"
 import { LuFolder, LuUser } from "react-icons/lu";
 import AboutTab from "./AboutTab";
 import BusinessesTab from "./BusinessesTab";
 
-const BusinessOwnerProfileTabs = ({businessOwnerData}) => {
-  return (
-   <Tabs.Root defaultValue="about">
-   <Tabs.List>
-     <Tabs.Trigger value="about">
-       <LuUser />
-       about
-     </Tabs.Trigger>
-     <Tabs.Trigger value="businesses">
-       <LuFolder />
-       Businesses
-     </Tabs.Trigger>
-   </Tabs.List>
-   
-   <Tabs.Content value="about"  >
-     <AboutTab businessOwnerData={businessOwnerData}/> 
-   </Tabs.Content>
-   
-   <Tabs.Content value="businesses">
-     <BusinessesTab businessOwnerId = {businessOwnerData?.business_owner_id}/>
-   </Tabs.Content>
-   
- </Tabs.Root>
-  )
+const BusinessOwnerProfileTabs = ({ businessOwnerData }) => {
+   return (
+      <Box paddingTop={2}>
+         <Tabs.Root defaultValue="about">
+            <Tabs.List>
+               <Tabs.Trigger value="about">
+                  <LuUser />
+                  about
+               </Tabs.Trigger>
+               <Tabs.Trigger value="businesses">
+                  <LuFolder />
+                  Businesses
+               </Tabs.Trigger>
+            </Tabs.List>
+
+            <Tabs.Content value="about"  >
+               <AboutTab businessOwnerData={businessOwnerData} />
+            </Tabs.Content>
+
+            <Tabs.Content value="businesses">
+               <BusinessesTab businessOwnerId={businessOwnerData?.business_owner_id} />
+            </Tabs.Content>
+
+         </Tabs.Root>
+      </Box>
+   )
 }
 
 export default BusinessOwnerProfileTabs;
